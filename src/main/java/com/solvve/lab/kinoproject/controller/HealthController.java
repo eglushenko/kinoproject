18db6b124f7e020edea4cc28f1629a08eb185167
Evgeni Glushenko <ev.glushenko@gmail.com>
2019-12-04 23:00:49 +0200

    Home-work-1

---
+++ src/main/java/com/solvve/lab/kinoproject/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.solvve.lab.kinoproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public void health() {
+
+    }
+
+
+}
---
+++ src/test/java/com/solvve/lab/kinoproject/controller/HealthControllerTest.java
@@ -0,0 +1,23 @@
+package com.solvve.lab.kinoproject.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HealthController.class)
+class HealthControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void testHealth() throws Exception {
+        mvc.perform(get("/health")).andExpect(status().isOk());
+    }
+}
