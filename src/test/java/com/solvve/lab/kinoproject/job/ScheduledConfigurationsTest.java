72facd7d7651b0db5aba617c144943fdc7bbd1c0
eglushenko <e.glushenko@hotmail.com>
2020-03-06 13:36:23 +0200

    Home-work-16

---
+++ src/test/java/com/solvve/lab/kinoproject/job/ScheduledConfigurationsTest.java
@@ -0,0 +1,30 @@
+package com.solvve.lab.kinoproject.job;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Import(ScheduledConfigurationsTest.ScheduledTestConfig.class)
+@ActiveProfiles("test")
+public class ScheduledConfigurationsTest {
+
+    @Test
+    public void testSpringContextUpAndRunning() {
+        log.info("@Scheduled confiuration is OK");
+    }
+
+    @EnableScheduling
+    static class ScheduledTestConfig {
+
+    }
+
+}
